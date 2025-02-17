3110a17b2d7769b7d7c7cb3ebc91ba4db3b8fcab
Calyx Hikari <hikaricalyx@hikaricalyx.com>
2025-02-17 13:19:20 +0800

    initial

---
+++ WzComparerR2.NetworkAI/Contracts/PackHeartBeat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace WzComparerR2.NetworkAI.Contracts
+{
+    [JsonObject("0")]
+    public sealed class PackHeartBeat
+    {
+    }
+}
