34681a7e4f6b4864d27b98cab65c93e7ad76d86a
Calyx Hikari <hikaricalyx@hikaricalyx.com>
2025-06-02 10:20:03 +0800

    Add CLI feature

---
+++ WzComparerR2.CLI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+#if NET6_0_OR_GREATER
+using System.Runtime.Loader;
+using System.Text;
+#endif
+
+namespace WzComparerR2.CLI
+{
+    class Program
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool AllocConsole();
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                PrintUsage();
+                return;
+            }
+            else
+            {
+                foreach (string arg in args)
+                {
+                    Console.WriteLine(arg);
+                }
+                Console.WriteLine("Hello, World!");
+            }
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Invalid argument");
+        }
+    }
+}
