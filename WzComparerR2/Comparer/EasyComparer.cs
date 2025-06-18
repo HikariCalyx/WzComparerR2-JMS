seotbeo/WzComparerR2

CharaSim: Show minimap icons on Map tooltip

WzComparerR2/Comparer/EasyComparer.cs
@@ -885,6 +885,9 @@ namespace WzComparerR2.Comparer
                 tooltipRenderNewOld[i].StringLinker = this.StringLinkerNewOld[i];
                 tooltipRenderNewOld[i].ShowObjectID = true;
                 tooltipRenderNewOld[i].ShowMiniMap = true;
+                tooltipRenderNewOld[i].ShowMiniMapMob = CharaSimConfig.Default.Map.ShowMiniMapMob;
+                tooltipRenderNewOld[i].ShowMiniMapNpc = true;
+                tooltipRenderNewOld[i].ShowMiniMapPortal = true;
                 tooltipRenderNewOld[i].SourceWzFile = WzFileNewOld[i];
             }
 

HikariCalyx/WzComparerR2-JMS

WzComparerR2/Comparer/EasyComparer.cs
@@ -1726,6 +1726,9 @@ namespace WzComparerR2.Comparer
                 mapRenderNewOld[i].Enable22AniStyle = this.Enable22AniStyle;
                 mapRenderNewOld[i].ShowObjectID = this.ShowObjectID;
                 mapRenderNewOld[i].ShowMiniMap = true;
+                mapRenderNewOld[i].ShowMiniMapMob = true;
+                mapRenderNewOld[i].ShowMiniMapNpc = true;
+                mapRenderNewOld[i].ShowMiniMapPortal = true;
                 mapRenderNewOld[i].ShowMobNpcObjectID = this.ShowObjectID;
             }
 