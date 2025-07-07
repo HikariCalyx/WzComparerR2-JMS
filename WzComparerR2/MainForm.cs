f1609d6637165cca27333c7f04967803c9a3bd13
seotbeo <yoonv72@gmail.com>
2025-07-07 14:00:31 +0900

    read origin property of wz video

--- WzComparerR2/MainForm.cs
+++ WzComparerR2/MainForm.cs
@@ -720,7 +720,8 @@ namespace WzComparerR2
             }
             else if (node.Value is Wz_Video)
             {
-                var videoFrameData = this.pictureBoxEx1.LoadVideo(node.Value as Wz_Video);
+                var origin = node.FindNodeByPath("origin").GetValueEx<Wz_Vector>(null);
+                var videoFrameData = this.pictureBoxEx1.LoadVideo(node.Value as Wz_Video, origin);
 
                 if (videoFrameData != null)
                 {
@@ -1659,9 +1660,11 @@ namespace WzComparerR2
                     break;
 
                 case Wz_Video video:
-                    textBoxX1.Text = "データ長:  " + video.Length + " バイ\r\n" +
+                    textBoxX1.Text = "データ長:  " + video.Length + " バイト\r\n" +
                         "オフセット:  " + video.Offset;
-                    var videoFrameData = this.pictureBoxEx1.LoadVideo(video);
+                    if (this.pictureBoxEx1.ShowOverlayAni) break; // 애니메이션 중첩 중일때는 자동 video 미리보기 없음
+                    var origin = selectedNode.FindNodeByPath("origin").GetValueEx<Wz_Vector>(null);
+                    var videoFrameData = this.pictureBoxEx1.LoadVideo(video, origin);
                     pictureBoxEx1.PictureName = GetSelectedNodeImageName();
                     this.pictureBoxEx1.ShowAnimation(videoFrameData);
                     this.cmbItemAniNames.Items.Clear();
