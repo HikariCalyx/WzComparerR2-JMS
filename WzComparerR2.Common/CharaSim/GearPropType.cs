71280285db69786aec263d631d2a1f8f0936a765
Calyx Hikari <hikaricalyx@hikaricalyx.com>
2025-07-29 12:53:38 +0800

    Add magic layer support

--- WzComparerR2.Common/CharaSim/GearPropType.cs
+++ WzComparerR2.Common/CharaSim/GearPropType.cs
@@ -259,6 +259,7 @@ namespace WzComparerR2.CharaSim
         limitedLabel,
         noPrism,
         noTradeLimitCash,
+        magicLayerWz2,
         //MSN exclusive stats
         blockUpgradeExtraOption,
         blockUpgradeStarforce,
