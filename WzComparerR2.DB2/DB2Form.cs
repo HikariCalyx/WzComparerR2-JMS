b3166b61d14551a6717a149602f1f677c3771905
Calyx Hikari <hikaricalyx@hikaricalyx.com>
2025-10-18 14:36:44 +0800

    Attempt to add as plugin

---
+++ WzComparerR2.DB2/DB2Form.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WzComparerR2.WzLib;
+
+namespace WzComparerR2.Db2
+{
+    public partial class DB2Form : Form
+    {
+        public DB2Form()
+        {
+            InitializeComponent();
+        }
+
+        public Wz_Node baseWzNode { get; set; }
+    }
+}
---
+++ WzComparerR2.DB2/Entry.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WzComparerR2.Config;
+using WzComparerR2.WzLib;
+using WzComparerR2.PluginBase;
+using DevComponents.DotNetBar;
+
+namespace WzComparerR2.Db2
+{
+    public class Entry : PluginEntry
+    {
+        public Entry(PluginContext context)
+            : base(context)
+        {
+            Instance = this;
+        }
+
+        internal static Entry Instance { get; private set; }
+
+        protected override void OnLoad()
+        {
+            var bar = this.Context.AddRibbonBar("Tools", "DB2");
+            ItemContainer db2Container1 = new ItemContainer();
+            ItemContainer db2Container1_0 = new ItemContainer();
+            ItemContainer db2Container1_1 = new ItemContainer();
+            ItemContainer db2Container1_2 = new ItemContainer();
+            ButtonItem btnItemSearchByIcon = new ButtonItem("btnItemSearchByIcon", "アイコンでアイテムを検索");
+            ButtonItem btnMapObjLookup = new ButtonItem("btnMapObjLookup", "マップオブジェクトを検索");
+            ButtonItem btnAssetImageViewer = new ButtonItem("btnAssetImageViewer", "アセットイメージビューア");
+            db2Container1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            db2Container1.LayoutOrientation = DevComponents.DotNetBar.eOrientation.Vertical;
+            db2Container1.Name = "db2Container1";
+            db2Container1.SubItems.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            db2Container1_0,
+            db2Container1_1,
+            db2Container1_2});
+
+            db2Container1_0.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            db2Container1_0.Name = "db2Container1_0";
+            db2Container1_0.SubItems.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            btnItemSearchByIcon});
+
+            db2Container1_1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            db2Container1_1.Name = "db2Container1_1";
+            db2Container1_1.SubItems.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            btnMapObjLookup});
+
+            db2Container1_2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            db2Container1_2.Name = "db2Container1_1";
+            db2Container1_2.SubItems.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            btnAssetImageViewer});
+
+            btnItemSearchByIcon.Click += btnItemSearchByIcon_Click;
+            bar.Items.Add(db2Container1);
+
+            ConfigManager.RegisterAllSection(this.GetType().Assembly);
+        }
+
+        DB2Form frm;
+
+        void btnItemSearchByIcon_Click(object sender, EventArgs e)
+        {
+            Wz_Node baseNode = Context.SelectedNode1;
+            if (baseNode == null || baseNode.GetNodeWzFile().Type != Wz_Type.Base)
+            {
+                MessageBoxEx.Show("ロードしたBase.wzを選択してください。", "注意");
+                return;
+            }
+            if (frm == null || frm.IsDisposed)
+            {
+                frm = new DB2Form();
+                frm.Owner = Context.MainForm;
+                frm.baseWzNode = baseNode;
+                
+            }
+            frm.Show();
+            frm.Focus();
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+        }
+    }
+}
