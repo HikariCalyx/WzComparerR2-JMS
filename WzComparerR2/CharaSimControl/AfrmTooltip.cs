33f7c3ef70cd12c0880292d442cc2a121b45de74
Calyx Hikari <hikaricalyx@hikaricalyx.com>
2025-12-31 22:31:01 +0800

    Remove unnecessary include

--- WzComparerR2/CharaSimControl/AfrmTooltip.cs
+++ WzComparerR2/CharaSimControl/AfrmTooltip.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
 using System.IO;
-using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
