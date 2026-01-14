2ce850a10cadf6d1aefafaedbc066b5190d0a697
Calyx Hikari <hikaricalyx@hikaricalyx.com>
2026-01-14 12:08:54 +0800

    Fix certain pet rendering issue

--- WzComparerR2/CharaSimControl/ItemTooltipRender2.cs
+++ WzComparerR2/CharaSimControl/ItemTooltipRender2.cs
@@ -849,18 +849,21 @@ namespace WzComparerR2.CharaSimControl
             {
                 Wz_Node petDialog = PluginManager.FindWz("String\\PetDialog.img\\" + item.ItemID);
                 Dictionary<string, int> commandLev = new Dictionary<string, int>();
-                foreach (Wz_Node commandNode in PluginManager.FindWz("Item\\Pet\\" + item.ItemID + ".img\\interact").Nodes)
+                if (PluginManager.FindWz("Item\\Pet\\" + item.ItemID + ".img\\interact", this.SourceWzFile) != null)
                 {
-                    foreach (string command in petDialog?.Nodes[commandNode.Nodes["command"].GetValue<string>()].GetValueEx<string>(null)?.Split('|') ?? Enumerable.Empty<string>())
+                    foreach (Wz_Node commandNode in PluginManager.FindWz("Item\\Pet\\" + item.ItemID + ".img\\interact", this.SourceWzFile).Nodes)
                     {
-                        int l0;
-                        if (!commandLev.TryGetValue(command, out l0))
+                        foreach (string command in petDialog?.Nodes[commandNode.Nodes["command"].GetValue<string>()].GetValueEx<string>(null)?.Split('|') ?? Enumerable.Empty<string>())
                         {
-                            commandLev.Add(command, commandNode.Nodes["l0"].GetValue<int>());
-                        }
-                        else
-                        {
-                            commandLev[command] = Math.Min(l0, commandNode.Nodes["l0"].GetValue<int>());
+                            int l0;
+                            if (!commandLev.TryGetValue(command, out l0))
+                            {
+                                commandLev.Add(command, commandNode.Nodes["l0"].GetValue<int>());
+                            }
+                            else
+                            {
+                                commandLev[command] = Math.Min(l0, commandNode.Nodes["l0"].GetValue<int>());
+                            }
                         }
                     }
                 }
