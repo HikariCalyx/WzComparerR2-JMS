c5639414f439adf8f459daf1b43ec3a4e224f525
Calyx Hikari <hikaricalyx@hikaricalyx.com>
2026-01-27 17:07:11 +0800

    Change background color to readable color

--- WzComparerR2/Comparer/EasyComparer.cs
+++ WzComparerR2/Comparer/EasyComparer.cs
@@ -4306,7 +4306,7 @@ namespace WzComparerR2.Comparer
                 sw.WriteLine("table.lst1 { }");
                 sw.WriteLine("table.lst2 { }");
                 sw.WriteLine("table.img tr.r0 { background-color:#CCCC00; color:#000000; }");
-                sw.WriteLine("table.img tr.r1 { background-color:#154211; }");
+                sw.WriteLine("table.img tr.r1 { background-color:#9CE34E; }");
                 sw.WriteLine("table.img tr.r2 { background-color:#961e1e; }");
                 sw.WriteLine("table.img.noChange { display:none; }");
             }
