828abe398e3486afd8d1d490a5c1ea60bc7b9e85
Calyx Hikari <hikaricalyx@hikaricalyx.com>
2026-04-02 13:34:56 +0800

    Comparer: Add Hyperlink color customization

--- WzComparerR2/MainForm.cs
+++ WzComparerR2/MainForm.cs
@@ -5379,7 +5379,8 @@ namespace WzComparerR2
                         CustomCSSConfig.Default.RemovedBackgroundColor,
                         CustomCSSConfig.Default.ChangedTextColor,
                         CustomCSSConfig.Default.AddedTextColor,
-                        CustomCSSConfig.Default.RemovedTextColor
+                        CustomCSSConfig.Default.RemovedTextColor,
+                        CustomCSSConfig.Default.HyperlinkColor
                     };
                     try
                     {
