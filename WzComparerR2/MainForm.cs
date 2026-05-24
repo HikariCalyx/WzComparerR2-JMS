9ecceb094a950d6b1b5bdb01d97c3325ce5d6c93
PirateIzzy <pirateizzy255@gmail.com>
2026-05-23 23:42:24 -0400

    Fixing a CSS issue with comparisons

--- WzComparerR2/MainForm.cs
+++ WzComparerR2/MainForm.cs
@@ -4804,7 +4804,8 @@ namespace WzComparerR2
                         CustomCSSConfig.Default.RemovedBackgroundColor,
                         CustomCSSConfig.Default.ChangedTextColor,
                         CustomCSSConfig.Default.AddedTextColor,
-                        CustomCSSConfig.Default.RemovedTextColor
+                        CustomCSSConfig.Default.RemovedTextColor,
+                        CustomCSSConfig.Default.HyperlinkColor
                     };
                     try
                     {
