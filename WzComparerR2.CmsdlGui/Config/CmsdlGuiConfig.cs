88f13dd54678f7fd02532c3fd40708612426ffbd
Calyx Hikari <29157608+HikariCalyx@users.noreply.github.com>
2026-07-02 19:45:18 +0800

    initialize cmsdlgui

---
+++ WzComparerR2.CmsdlGui/Config/CmsdlGuiConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WzComparerR2.Config;
+
+namespace WzComparerR2.CmsdlGui.Config
+{
+    [SectionName("WcR2.CmsdlGui")]
+    public class CmsdlGuiConfig : ConfigSectionBase<CmsdlGuiConfig>
+    {
+        public CmsdlGuiConfig()
+        {
+        }
+
+        /// <summary>
+        /// Previously selected region
+        /// </summary>
+        [ConfigurationProperty("selectedRegion")]
+        public ConfigItem<int> SelectedRegion
+        {
+            get { return (ConfigItem<int>)this["selectedRegion"]; }
+            set { this["selectedRegion"] = value; }
+        }
+
+        /// <summary>
+        /// CMS client path
+        /// </summary>
+        [ConfigurationProperty("cmsPath")]
+        public ConfigItem<string> CmsPath
+        {
+            get { return (ConfigItem<string>)this["cmsPath"]; }
+            set { this["cmsPath"] = value; }
+        }
+
+
+        /// <summary>
+        /// TMS client path
+        /// </summary>
+        [ConfigurationProperty("tmsPath")]
+        public ConfigItem<string> TmsPath
+        {
+            get { return (ConfigItem<string>)this["tmsPath"]; }
+            set { this["tmsPath"] = value; }
+        }
+    }
+
+}
---
+++ WzComparerR2.CmsdlGui/FrmCmsdlGui.cs
@@ -0,0 +1,15 @@
+﻿using DevComponents.AdvTree;
+using DevComponents.DotNetBar;
+using DevComponents.Editors;
+using System.Windows.Forms;
+
+namespace WzComparerR2.CmsdlGui
+{
+    public partial class FrmCmsdlGui : DevComponents.DotNetBar.Office2007Form
+    {
+        public FrmCmsdlGui()
+        {
+            InitializeComponent();
+        }
+    }
+}
